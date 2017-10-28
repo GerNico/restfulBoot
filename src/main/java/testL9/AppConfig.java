ce8557f89add46042cd1d13bf4e46c84e4599fdd
GerNico <matsishinnicolas@gmail.com>
2017-10-28 12:52:26 +0300

    pre

--- src/main/java/testL9/AppConfig.java
+++ src/main/java/testL9/AppConfig.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppConfig {
 
     public static void main(String[] args) {
-        SpringApplication.run(AppConfig.class,args);
+        SpringApplication.run(AppConfig.class, args);
     }
 }
